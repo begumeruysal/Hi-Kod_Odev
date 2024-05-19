dace89756b4cd1304dfcc9f8fdc86065b51fef61
begumeruysal <begumeruysal@gmail.com>
2024-05-19 14:00:04 +0300

    odev1

---
+++ odev_1/android/Odev_1.dart
@@ -0,0 +1,23 @@
+void main() {
+  int yil = 2024;
+  String isim = "Begüm";
+  double fiyat = 189.65;
+  bool doru = true;
+  dynamic deger = "Dinamik";
+  String harf = 'A';
+  //print("Yıl: $yil, İsim: $isim, Fiyat: $fiyat, Doğru: $doru, Değer: $deger, Harf: $harf");
+  deger = 34;
+  //print("Değer:$deger");
+
+  String kullaniciIsmi = "Begüm";
+  double urun_fiyati = 189.65;
+  dynamic KullanilanDeger = 134.6;
+  //print("$kullaniciIsmi,$urun_fiyati,$KullanilanDeger");
+
+  String ad = "Begüm Işık";
+  String soyad = "Eruysal";
+  int yas = 21;
+  bool resit = true;
+  print("Ben $ad $soyad yaşım $yas reşitlik durumum $resit");
+
+}
